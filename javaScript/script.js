bd497f58b38d08924f5c101a2c921ef00ca5c80d
Clécio Sousa <54221604+cleciosousa@users.noreply.github.com>
2023-02-14 15:08:29 -0300

    update

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,10 +1,21 @@
 let menuIcon = document.querySelector('#hero__icon-menu')
 let closeIcon = document.querySelector('#box__icon-close')
 let menuBox = document.querySelector('.hero__box')
+
+
+
+
 menuIcon.addEventListener('click',()=>{
     menuBox.classList.toggle('active')
 })
 
-closeIcon.addEventListener('click',()=>{
-    console.log('outro clic');
-})
+
+// boxFilmes.addEventListener('wheel', event =>{
+//     if(event.deltaY > 0){
+//         event.target.scrollBy(100,0)
+//     }else{
+//         event.target.scrollBy(0,100)
+//     }
+// })
+
+
